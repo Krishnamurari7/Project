64d239cfdbfdf6c3f4ddd13c5c874f09bbece054
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-02-01 17:27:05 +0530

    finished

--- Api_use/script.js
+++ Api_use/script.js
@@ -1,8 +1,18 @@
-const URL = "https://cat-fact.herokuapp.com/facts";
+async function apiuse() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const result = await response.json();
+//   console.log(result);
+document.getElementById("userData").innerHTML = result.map((user) => 
+        `<tr>
+        <td>${user.name}</td>
+        <td>${user.phone}</td>
+        <td>${user.username}</td>
+        <td>${user.website}</td>
+        <td>${user.email}</td>
+        </tr>`
+        )
 
-
-const getFact = async() => {
-    console.log("loading...");
-const response = await fetch(URL);
-console.log(response);
+ .join("");
 };
+
+apiuse();

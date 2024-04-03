874b6300408c4a216d480a17288af593dbad84a9
Krishna murari kumar <ayaankumar7488@gmail.com>
2024-04-03 14:14:26 +0530

    update and run

---
+++ background_change/script .js
@@ -0,0 +1,28 @@
+const hero = document.querySelector('main');
+
+//background color change
+
+// function backgroundChange()
+//  {
+//     hero.style.color= 'black';  
+//  }
+
+
+
+ //random backgroun color change
+//   function ColorChange()
+//   { 			
+//     var rndCol = '#' + Math.floor(Math.random()*16777215);		
+//      hero.style.backgroundColor = rndCol;   
+//   }
+ 
+  document.getElementById('changeColorBtn').addEventListener('click', function() {
+    // Generate random RGB color values
+    var randomColor = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+  
+    // Apply the random color as background color
+    document.body.style.backgroundColor = randomColor;
+    console.log(randomColor);
+    document.body.appendChild(hero);
+  });
+  
